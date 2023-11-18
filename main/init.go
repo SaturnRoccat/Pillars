0009adf1ccde1d93264404cbe8b52ec2d0557481
Duckos-Mods <businwes@hotmail.com>
2023-11-18 21:34:21 +0000

    Added Tempt Dir

--- main/init.go
+++ main/init.go
@@ -11,6 +11,7 @@ type ProjectConfig struct {
 	ProjectName string `json:"projectName"`
 	BPPath      string `json:"bpPath"`
 	RPPath      string `json:"rpPath"`
+	TempPath    string `json:"tempPath"`
 }
 
 type ProjectFileNameAndEditTime struct {
@@ -58,6 +59,12 @@ func createDefaultState(pathToRoot string, projectName string) error {
 		return err
 	}
 
+	// Make the Pillars/Temp directory
+	err = os.Mkdir(fmt.Sprintf("%s/Pillars/Temp", pathToRoot), 0777)
+	if err != nil {
+		return err
+	}
+
 	// Create the RP and BP directories
 	err = os.Mkdir(fmt.Sprintf("%s/%s_Bp", pathToRoot, projectName), 0777)
 	if err != nil {
@@ -79,6 +86,7 @@ func createDefaultState(pathToRoot string, projectName string) error {
 		ProjectName: projectName,
 		BPPath:      fmt.Sprintf("%s/%s_Bp", pathToRoot, projectName),
 		RPPath:      fmt.Sprintf("%s/%s_Rp", pathToRoot, projectName),
+		TempPath:    fmt.Sprintf("%s/Pillars/Temp", pathToRoot),
 	}
 	// Write the default config to the ProjectConfig.json file
 	writeDefaultConfig(fmt.Sprintf("%s/Pillars/ProjectConfig.json", pathToRoot), defaultConfig)
