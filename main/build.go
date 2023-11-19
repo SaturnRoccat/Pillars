ba22687a5ce0a6e796fd5a42105c98a9d8a99059
Duckos-Mods <businwes@hotmail.com>
2023-11-19 03:21:09 +0000

    added a working build function

--- main/build.go
+++ main/build.go
@@ -1,31 +1,57 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/duckos-Mods/Pillars/main/PC"
 )
 
-func build(sources, targets []string, bypassCache bool, pathToTemp string) error {
+func build(pathToRoot string) error {
+	// Get project config
+	var config, err = PC.PullJson[PC.ProjectConfig](pathToRoot + "/Pillars/ProjectConfig.json")
+	if err != nil {
+		return err
+	}
+	return buildAddon(
+		[]string{config.BPPath, config.RPPath},
+		[]string{fmt.Sprintf("%s/development_behavior_packs/%s_BP", MCBEPath, config.ProjectName), fmt.Sprintf("%s/development_resource_packs/%s_RP", MCBEPath, config.ProjectName)},
+		false,
+		pathToRoot+"/Pillars/Temp",
+	)
+}
 
-	for _, source := range sources {
-		for _, target := range targets {
-			// get the files to ignore
-			var filesToIgnore = getFilesToIgnore(source)
+func buildAddon(sources, targets []string, bypassCache bool, pathToTemp string) error {
 
-			// Copy all files to the temp dir
-			var err = PC.BulkFileCopy(source, pathToTemp+"/Pillars/Temp", filesToIgnore)
-			if err != nil {
-				return err
-			}
+	for i := 0; i < len(sources); i++ {
+		source := sources[i]
+		target := targets[i]
+		// get the files to ignore
+		var filesToIgnore = getFilesToIgnore(source)
 
-			/*
-				TODO:
-					- Call any external plugins that request to be called on build (e.g. a plugin that minifies files)
-			*/
+		// Copy all files to the temp dir
+		var err = PC.BulkFileCopy(source, pathToTemp, filesToIgnore)
+		if err != nil {
+			return err
+		}
 
-			// Copy all files from the temp dir to the build dir
-			err = PC.BulkFileCopy(source, target, nil)
+		/*
+			TODO:
+				- Call any external plugins that request to be called on build (e.g. a plugin that minifies files)
+		*/
+
+		println("Done building. For " + target + "!...")
+		// Copy all files from the temp dir to the build dir
+		// err = PC.BulkFileCopy(source, target, nil)
+
+		// clear the temp dir
+
+		err = PC.DeleteContents(pathToTemp)
+		if err != nil {
+			return err
 		}
 	}
 
 	return nil
 }
+
+// D:\\Projects\\Go\\Pillars\\CompilerTests/Pillars/Temp
