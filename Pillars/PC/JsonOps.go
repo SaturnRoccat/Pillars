d81ceab124e40ea1d7eb7d50f56d7b8645235baa
Duckos-Mods <businwes@hotmail.com>
2023-11-20 16:18:54 +0000

    Added a use for JsonOps file

--- Pillars/PC/JsonOps.go
+++ Pillars/PC/JsonOps.go
@@ -1,1 +1,29 @@
 package PC
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func PullJson[T interface{}](PTJ string) (T, error) {
+	var config T
+	var jsonData []byte
+	var err error
+
+	jsonData, err = os.ReadFile(PTJ)
+	if err != nil {
+		return config, err
+	}
+
+	err = json.Unmarshal(jsonData, &config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
+func WriteEmptyJson(PTJ string) error {
+
+	return os.WriteFile(PTJ, []byte("{}"), 0777)
+}
